b7e6cc8d323761e0aded0515f58dd2e319f9b103
Alan Alvarenga <alan.alvarengamejia@telus.com>
2022-01-23 21:21:40 -0600

    removing key

--- src/config/firebase.config.jsx
+++ src/config/firebase.config.jsx
@@ -4,13 +4,7 @@
  */
 
 const FIREBASE = {
-  apiKey: "AIzaSyA48ZrYTnVoA98oWsjktZtb-9m0KdYQ_oc",
-  authDomain: "encuesta-tinmarin-946c0.firebaseapp.com",
-  projectId: "encuesta-tinmarin",
-  storageBucket: "encuesta-tinmarin.appspot.com",
-  messagingSenderId: "965942954093",
-  appId: "1:965942954093:web:e9024e25a619add3989764",
-  measurementId: "G-P2GYMY4SXY"
+
 }
 
 export default FIREBASE
